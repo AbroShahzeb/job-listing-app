dc9229074ebe0062639631d211d6993241137293
AbroShahzeb <khizr772@gmail.com>
2023-08-30 09:59:33 -0700

    Set up deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="w-full min-h-screen grid place-content-center text-4xl font-bold bg-emerald-800 text-slate-50">
-      A basic vite + react app
+      A basic vite + react app (Job Listing App)
     </div>
   );
 }
