8d91f927092cc99ffb511b2c8b53c5efbeacedb6
AbroShahzeb <khizr772@gmail.com>
2023-08-30 09:56:50 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full min-h-screen grid place-content-center text-4xl font-bold bg-emerald-800 text-slate-50">
+      A basic vite + react app
+    </div>
+  );
+}
+
+export default App;
